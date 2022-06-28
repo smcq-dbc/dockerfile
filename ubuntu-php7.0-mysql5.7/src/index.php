d405e93dfc40db9ba8a62cde66c290dd718c3017
smcq-dbc <2239175123@qq.com>
2022-06-28 13:23:03 +0800

    增加mysql5.7版本环境

---
+++ ubuntu-php7.0-mysql5.7/src/index.php
@@ -0,0 +1,15 @@
+<?php
+$dbhost = 'localhost';  // mysql服务器主机地址
+$dbuser = 'root';            // mysql用户名
+$dbpass = 'smcq123';          // mysql用户名密码
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+    die('Could not connect: ' . mysqli_error());
+}
+echo '数据库连接成功！';
+mysqli_close($conn);
+?>
+<?php
+phpinfo();
+?>
